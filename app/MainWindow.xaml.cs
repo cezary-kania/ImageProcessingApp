68224caf4091313efc181c59dde5d06933a9fd36
Cezary Kania <czainaktv@gmail.com>
2020-03-05 14:52:03 +0100

    App init

---
+++ app/MainWindow.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace APO_v1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private List<MyImage> images = new List<MyImage>();
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OpenBtn_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                MyImage newImg = new MyImage(new BitmapImage(new Uri(openFileDialog.FileName)));
+                images.Add(newImg);
+            }
+                
+        }
+        
+    }
+}
---
+++ app/MyImage.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows.Media.Imaging;
+using Microsoft.Win32;
+using System.Windows;
+namespace APO_v1
+{
+    class MyImage
+    {
+        public MyImage(BitmapImage bitmapImage)
+        {
+            ImageWindow newImageW = new ImageWindow();
+            newImageW.Show();
+
+            BitmapImage image = bitmapImage;
+            newImageW.image.Source = image;
+
+            int height = image.PixelHeight;
+            int width = image.PixelWidth;
+
+            newImageW.Width = width;
+            newImageW.Height = height;
+
+            int stride = width * 4;
+            int size = height * stride;
+            byte[] pixels = new byte[size];
+            image.CopyPixels(pixels, stride, 0);
+
+            int x = 0, y = 0;
+            int index = y * stride + 4 * x;
+
+            newImageW.Title = pixels[index] + " " + pixels[index + 1] + " " + pixels[index + 2] + " " + pixels[index + 3];
+        }
+    }
+}
